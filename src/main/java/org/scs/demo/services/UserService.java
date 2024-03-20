66a949a5a390fee78d3a6c7a925d91baeff06e1c
hjk81371 <hjk81371@uga.edu>
2024-03-19 20:58:41 -0400

    added password field vulnerability

--- src/main/java/org/scs/demo/services/UserService.java
+++ src/main/java/org/scs/demo/services/UserService.java
@@ -29,23 +29,33 @@ public class UserService {
 
 
     public boolean authenticate(String username, String password) throws SQLException {
-        final String sql = "SELECT * FROM user WHERE username = '" + username + "'";
-        System.out.println("USERNAME: " + username);
+        final String sql = "SELECT * FROM user WHERE username = '" + username + "' AND password = '" + password + "'";
+        System.out.println("SQL QUERY: " + sql);
+        // System.out.println("USERNAME: " + username);
         try (Connection conn = dataSource.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
     
             try (ResultSet rs = pstmt.executeQuery()) {
                 while (rs.next()) {
-                    String storedPassword = rs.getString("password");
-                    boolean isPassMatch = password.equals(storedPassword);
+                    // System.out.println("HERE");
+                    // String puid = rs.getString("userId");
+                    // String puname = rs.getString("username");
+                    // String ppw = rs.getString("password");
+                    
+                    // Print out the retrieved data
+                    // System.out.println("User ID: " + puid + ", Username: " + puname + ", Password: " + ppw);
+
+                    // String storedPassword = rs.getString("password");
+                    // boolean isPassMatch = password.equals(storedPassword);
+                    boolean isPassMatch = true;
                     if (isPassMatch) {
                         String userId = rs.getString("userId");
                         String uname = rs.getString("username");
                         String pw = rs.getString("password");
     
                         loggedInUser = new User(userId, uname, pw);
+                        return true;
                     }
-                    return isPassMatch;
                 }
             }
         }
